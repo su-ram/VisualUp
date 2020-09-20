1c4803adc843959806a239040ae1fd45b29420c1
Cho-Eunhak <02069@naver.com>
2020-09-21 02:00:42 +0900

    no message

--- FrontEnd/src/pages/GoalList.js
+++ FrontEnd/src/pages/GoalList.js
@@ -1,11 +1,35 @@
 import React from 'react';
-
+import { Card, Col, Row, Pagination } from 'antd';
+import {  EllipsisOutlined } from '@ant-design/icons';
 function GoalList(){
     return(
-        <div>
-            GoalList
-        </div>
-    );
+        <div className="site-card-wrapper">
+    <Row justify="center">
+      <Col span={8}>
+        <Card style={{ width: 300 }} title="Card title" bordered={true} actions={[
+      <EllipsisOutlined key="ellipsis" />,
+    ]}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card style={{ width: 300 }} title="Card title" bordered={true} actions={[
+      <EllipsisOutlined key="ellipsis" />,
+    ]}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card style={{ width: 300 }} title="Card title" bordered={true} actions={[
+      <EllipsisOutlined key="ellipsis" />,
+    ]}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+
+    <Pagination defaultCurrent={1} total={50} />
+  </div>)
 }
 
 export default GoalList;
---
+++ FrontEnd/src/pages/NoGoal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NoGoal(){
+    return(
+        <div>
+            아직 홍미주 님의 목표가 등록되지 않았어요!
+
+            
+        </div>
+    );
+}
+
+export default NoGoal;
