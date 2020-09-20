82e822babf684072467380a06a648ff634742d5e
heeeun <01072417732w@gmail.com>
2020-09-20 22:49:04 +0900

    Add routing about Goal

---
+++ FrontEnd/src/pages/GoalList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GoalList(){
+    return(
+        <div>
+            GoalList
+        </div>
+    );
+}
+
+export default GoalList;
--- FrontEnd/src/pages/Main.js
+++ FrontEnd/src/pages/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Header } from '../components';
-import { HashTag, Visualize } from '../pages';
+import { HashTag, Visualize, GoalList, GoalSet } from '../pages';
 //import { Line } from '@ant-design/charts';
 
 function Main(){
@@ -12,6 +12,8 @@ function Main(){
             <Switch>
                 <Route exact path="/hashtag" component={HashTag}/>
                 <Route exact path="/visualize" component={Visualize}/>
+                <Route exact path="/goalList" component={GoalList}/>
+                <Route exact path="/goalSet" component={GoalSet}/>
             </Switch>
         </div>
     );
