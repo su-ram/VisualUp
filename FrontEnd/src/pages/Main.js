dd55352164febfe8929d0d4310faa295d6114067
heeeun <01072417732w@gmail.com>
2020-09-27 19:46:46 +0900

    Set root container of components margin 10%

--- FrontEnd/src/pages/Main.js
+++ FrontEnd/src/pages/Main.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Header } from '../components';
 import { HashTag, Visualize, GoalList, GoalSet } from '../pages';
-//import { Line } from '@ant-design/charts';
+import { Col } from 'antd';
+import "./Main.css"
 
 function Main(){
     return(
-        <div>
+        <Col>
             <Header/>
-            Main
-            <Switch>
-                <Route exact path="/hashtag" component={HashTag}/>
-                <Route exact path="/visualize" component={Visualize}/>
-                <Route exact path="/goalList" component={GoalList}/>
-                <Route exact path="/goalSet" component={GoalSet}/>
-                <Redirect path="*" to="/error" />
-            </Switch>
-        </div>
+            
+            <Col id="contents">
+                <Switch>
+                    <Route exact path="/" component={Visualize}/>
+                    <Route exact path="/hashtag" component={HashTag}/>
+                    <Route exact path="/goalList" component={GoalList}/>
+                    <Route exact path="/goalSet" component={GoalSet}/>
+                    <Redirect path="*" to="/error" />
+                </Switch>
+            </Col>
+        </Col>
     );
 }
 
