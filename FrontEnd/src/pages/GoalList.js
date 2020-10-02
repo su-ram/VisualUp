63a14329eaa67c358cfefc189e838fbd3a5c839d
Cho-Eunhak <02069@naver.com>
2020-10-03 07:39:46 +0900

    목표리스트, 목표설정 페이지 수정

--- FrontEnd/src/pages/GoalList.js
+++ FrontEnd/src/pages/GoalList.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import { Card, Col, Row, Pagination } from 'antd';
 import {  EllipsisOutlined } from '@ant-design/icons';
+import styles from './GoalList.module.css';
 function GoalList(){
     return(
-        <div className="site-card-wrapper">
-    <Row justify="center">
-      <Col span={8}>
+      <div>
+            <h1 className={styles.mainTitle}>홍미주 님의 목표 리스트</h1>
+            <Col align="middle" className={styles.sub}><p className={styles.subSub}>본인이 계획한 모든 목표들을 한 눈에 볼 수 있어요</p></Col>
+            <Row align="middle" className={styles.body} justify="center">
+      <Col span={6}>
         <Card style={{ width: 300 }} title="Card title" bordered={true} actions={[
       <EllipsisOutlined key="ellipsis" />,
     ]}>
           Card content
         </Card>
       </Col>
-      <Col span={8}>
+      <Col span={6}>
         <Card style={{ width: 300 }} title="Card title" bordered={true} actions={[
       <EllipsisOutlined key="ellipsis" />,
     ]}>
           Card content
         </Card>
       </Col>
-      <Col span={8}>
+      <Col span={6}>
         <Card style={{ width: 300 }} title="Card title" bordered={true} actions={[
       <EllipsisOutlined key="ellipsis" />,
     ]}>
           Card content
         </Card>
       </Col>
-    </Row>
+      <Col span={6}>
+        <Card style={{ width: 300 }} title="Card title" bordered={true} actions={[
+      <EllipsisOutlined key="ellipsis" />,
+    ]}>
+          Card content
+        </Card>
+      </Col>
+            </Row>
 
-    <Pagination defaultCurrent={1} total={50} />
-  </div>)
+      </div>)
 }
 
 export default GoalList;
--- FrontEnd/src/pages/GoalSet.js
+++ FrontEnd/src/pages/GoalSet.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Input, Col, Row, Checkbox, InputNumber, DatePicker } from 'antd';
 import "antd/dist/antd.css";
-//import './GoalSet.css';
+import styles from './GoalSet.module.css';
 import { LineChartOutlined, BarChartOutlined, PieChartOutlined, AlignLeftOutlined, EditOutlined, CheckOutlined, CalendarOutlined, BarsOutlined, PushpinOutlined } from '@ant-design/icons'
 
 
@@ -15,98 +15,83 @@ function onChange(e) {
 
 function GoalSet(){
     return(
+        <div>
+        <h1 className={styles.mainTitle}>목표 입력</h1>
+            <Col align="middle" className={styles.sub}><p className={styles.subSub}>본인이 달성할 목표를 상세하게 적어주세요.</p></Col>
         <Row justify="center" >
             <Col span={9} offset={3}>
-                    <Input.Group size="large" className="GoalSet-input">
+                    <Input.Group className="GoalSet-input">
                         <Col span={13}>
-                        <EditOutlined />
-                            목표
+                        <p className={styles.name}><EditOutlined />목표</p>
                         <br />
                         <Input placeholder="ex)코딩 테스트 풀기"/>
                         </Col>
                         <br />
                         <br />
-                        <br />
-                        <br />
                         <Col span={13}>
-                        <CheckOutlined />
-                            목표량
-                            <br />
+                        <p className={styles.name}><CheckOutlined />목표량</p>
+                        <br />
                         <Input placeholder="3"/>
                         </Col>
                         <br />
                         <br />
+                        <Col span={15}>
+                        <p className={styles.name}><CalendarOutlined />날짜 선택</p>
                         <br />
                         <br />
-                    <Input.Group compact>
-                    <CalendarOutlined />
-                        날짜 선택
-                        <br />
-                        <DatePicker.RangePicker style={{ width: '70%' }} />
-                    </Input.Group>
-                        <br />
-                        <br />
+                        <DatePicker.RangePicker style={{ width: '100%' }} />
+                        </Col>
                         <br />
                         <Col span={13}>
-                            <BarsOutlined />
-                            주기
-                        <InputNumber min={1} max={10} defaultValue={3} onChange={onDateChange} />일마다
+                        <p className={styles.name}><BarsOutlined />주기
+                        <br />
+                        <InputNumber min={1} max={10} defaultValue={3} onChange={onDateChange} />
+                        일마다
+                        </p>
                          </Col>
                     </Input.Group>
             
             </Col>
-            <Col span={9}>
-                <Col span={15}>
-                <PushpinOutlined />
-                     해시태그 설정
-                     <br />
-                    <Input placeholder="ex)코테"/>
+            <Col span={12}>
+                <Col span={16}>
+                <p className={styles.name}><PushpinOutlined />해시태그 설정</p>
+                     <br /><br /><br /><br />
                 </Col>
-                <br/>
+                <Row>
+                    <Col span={8}>
+                        <Input placeholder="ex)코테"/>
+                        <Input placeholder="ex)C++"/>
+                    </Col>
+                    <br />
+                    <br />
+                    <Col span={8}>
+                        <Input placeholder="ex)취준"/>
+                        <Input placeholder="ex)취준"/>
+                    </Col>
+                </Row>
                 <Col span={15}>
-                    <Input placeholder="ex)C++"/>
+                <div className={styles.name}><AlignLeftOutlined />템플릿 설정</div>
                 </Col>
-                <br/>
-                <Col span={15}>
-                    <Input placeholder="ex)취준"/>
-                </Col>
-                <br/>
-                <Col span={15}>
-                    <Input placeholder="ex)파이썬"/>
-                </Col>
-                <br/>
-                <Col span={15}>
-                    <Input placeholder="ex)자바"/>
-                </Col>
-                <br />
-                <AlignLeftOutlined />
-                템플릿 설정
-                <br />
+                <Col span={10}>
                 <Checkbox onChange={onChange}>
-                파이 그래프
-                <br/>
-                얼마나 많은 비중을 두고 했는지 볼 수 있어요</Checkbox>
-                <br/>
-                <PieChartOutlined style={{ fontSize: '40px'}}/>
-                <br/>
-                <br/>
+                <div className={styles.graphname}>파이 그래프</div>
+                <PieChartOutlined/>
+                <div className={styles.graphdesc}>얼마나 많은 비중을 두고 했는지 볼 수 있어요</div>
+                </Checkbox>
                 <Checkbox onChange={onChange}>
-                선 그래프
-                <br/>
-                일일 목표 달성률을 한 눈에 볼 수 있어요</Checkbox>
-                <br/>
-                <LineChartOutlined style={{ fontSize: '40px'}} />
-                <br/>
-                <br/>
+                <div className={styles.graphname}>선 그래프</div>
+                <PieChartOutlined style={{ fontSize: '20px'}}/>
+                <div className={styles.graphdesc}>일일 목표 달성률을 한 눈에 볼 수 있어요</div>
+                </Checkbox>
                 <Checkbox onChange={onChange}>
-                
-                막대 그래프
-                <br/> 목표 달성 순위를 매길 수 있어요. </Checkbox>
-                <br/>
-                <BarChartOutlined style={{ fontSize: '40px'}}/>
-                
+                <div className={styles.graphname}>막대 그래프</div>
+                <PieChartOutlined style={{ fontSize: '20px'}}/>
+                <div className={styles.graphdesc}>목표 달성 순위를 매길 수 있어요</div>
+                </Checkbox>
+                </Col>
             </Col>
             </Row>
+            </div>
     );
 }
 
