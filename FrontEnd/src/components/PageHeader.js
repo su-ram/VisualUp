1eaafc63f9157dbe1919d6c570fc4294dd135ef4
heeeun <01072417732w@gmail.com>
2020-10-02 13:37:55 +0900

    complete PageHeader frame

--- FrontEnd/src/components/PageHeader.js
+++ FrontEnd/src/components/PageHeader.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Col, Typography } from 'antd';
 import "./PageHeader.css";
-
-const {Title} = Typography;
 
 function PageHeader(props){
 
     return (
-        <Col className="page-title-con">
-            <Title level={2} className="page-title">{props.title}</Title>
-            <Title level={5} className="page-subtitle">{props.subtitle}</Title>
-        </Col>
+        <div className="page-title-con">
+            <div className="page-title"><h2>{props.title}</h2></div>
+            <div className="page-subtitle"><h5>{props.subtitle}</h5></div>
+        </div>
     );
 }
 
