2ad9cb939febf6588223cf39f9a9db1e8e3879af
heeeun <01072417732w@gmail.com>
2020-10-04 13:53:12 +0900

    visaulizing graph

--- FrontEnd/src/pages/Visualize.js
+++ FrontEnd/src/pages/Visualize.js
@@ -1,416 +1,371 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { PageHeader } from '../components';
-import { Line } from '@ant-design/charts';
-import { Col, DatePicker } from 'antd';
+import { Area, Line, Bar } from '@ant-design/charts';
+import { Col, DatePicker, Tabs, Button } from 'antd';
 import { CalendarOutlined } from '@ant-design/icons';
+import moment from 'moment';
+import "./Visualize.css";
 
-function Visualize(){
+const { TabPane } = Tabs;
+
+function Visualize() {
+  const range = 0.6; // 나중에 비율 조정
   const [data, setData] = useState([]);
+  const [graphDate, setGraphDate] = useState(undefined);
+  const [graphRate, setGraphRate] = useState(1 - range / 2);
   const config = {
     data,
-    padding: 'auto',
-    xField: 'Date', // xfield에 적용할 변수
-    yField: 'scales', // yfield에 적용할 변수
-    //seriesField: 'category', // 클릭 시 해당 그래프만 나타남
-    legend: { position: 'left' }, // seriesField 위치,
-    shared: false,
-    xAxis:{
-      type: 'dateTime', // x축에 표시되는 양식
-      tickCount: 5, // 몇 조각으로 나눌 건지
+    //width: '100',
+    xField: 'date', // xfield에 적용할 변수
+    yField: 'value', // yfield에 적용할 변수
+    forceFit: true,
+    color: ['#E9BE78', '#6a93c5', '#6ac593'], // 선 색깔 지정
+    seriesField: 'type', // 클릭 시 해당 그래프만 나타남
+    legend: {
+      //visible: false,
+      //legend: { position: 'bottom-left' },
     },
-    slidable: true,
-    slider:{
-      start : 0.1, 
-      end : 0.5
+    xAxis: {
+      type: 'dateTime', // x축 표시 형식
+      tickCount: 10, // 몇 조각으로 나눌 건지
     },
-    //color: ['#E9BE78', '#6a93c5', '#6ac593'], // 선 색깔 지정
-    point : {
-      size: 5,
+    yAxis: { formatter: (v) => `${v}%` }, // y축 표시 형식
+    interactions: [
+      {
+        type: 'slider',
+        cfg: {
+          start: graphRate - range / 2,
+          end: graphRate + range / 2,
+        },
+      },
+    ],
+    label: {
+      visible: true,
+      type: 'line',
+    },
+    point: {
+      visible: true,
+      size: 3,
       shape: 'circle',
-      style:{
-        fill: 'white',
-        stroke : '#2593fc',
+      style: {
+        fill: 'transparent',
+        stroke: 'white',
         lineWidth: 2,
-      }
-    }
+      },
+    },
+    //smooth: true, // 그래프 부드럽게
   };
 
   const name = "홍미주";
+  const operations = <Button onClick={gotoGoalSet}>Add Goal</Button>;
 
-  useEffect(()=>{
-    function defaultSetting(){
-      setData([
-        {
-          "Date": "2010-01",
-          "scales": 1998
-        },
-        {
-          "Date": "2010-02",
-          "scales": 1850
-        },
-        {
-          "Date": "2010-03",
-          "scales": 1720
-        },
-        {
-          "Date": "2010-04",
-          "scales": 1818
-        },
-        {
-          "Date": "2010-05",
-          "scales": 1920
-        },
-        {
-          "Date": "2010-06",
-          "scales": 1802
-        },
-        {
-          "Date": "2010-07",
-          "scales": 1945
-        },
-        {
-          "Date": "2010-08",
-          "scales": 1856
-        },
-        {
-          "Date": "2010-09",
-          "scales": 2107
-        },
-        {
-          "Date": "2010-10",
-          "scales": 2140
-        },
-        {
-          "Date": "2010-11",
-          "scales": 2311
-        },
-        {
-          "Date": "2010-12",
-          "scales": 1972
-        },
-        {
-          "Date": "2011-01",
-          "scales": 1760
-        },
-        {
-          "Date": "2011-02",
-          "scales": 1824
-        },
-        {
-          "Date": "2011-03",
-          "scales": 1801
-        },
-        {
-          "Date": "2011-04",
-          "scales": 2001
-        },
-        {
-          "Date": "2011-05",
-          "scales": 1640
-        },
-        {
-          "Date": "2011-06",
-          "scales": 1502
-        },
-        {
-          "Date": "2011-07",
-          "scales": 1621
-        },
-        {
-          "Date": "2011-08",
-          "scales": 1480
-        },
-        {
-          "Date": "2011-09",
-          "scales": 1549
-        },
-        {
-          "Date": "2011-10",
-          "scales": 1390
-        },
-        {
-          "Date": "2011-11",
-          "scales": 1325
-        },
-        {
-          "Date": "2011-12",
-          "scales": 1250
-        },
-        {
-          "Date": "2012-01",
-          "scales": 1394
-        },
-        {
-          "Date": "2012-02",
-          "scales": 1406
-        },
-        {
-          "Date": "2012-03",
-          "scales": 1578
-        },
-        {
-          "Date": "2012-04",
-          "scales": 1465
-        },
-        {
-          "Date": "2012-05",
-          "scales": 1689
-        },
-        {
-          "Date": "2012-06",
-          "scales": 1755
-        },
-        {
-          "Date": "2012-07",
-          "scales": 1495
-        },
-        {
-          "Date": "2012-08",
-          "scales": 1508
-        },
-        {
-          "Date": "2012-09",
-          "scales": 1433
-        },
-        {
-          "Date": "2012-10",
-          "scales": 1344
-        },
-        {
-          "Date": "2012-11",
-          "scales": 1201
-        },
-        {
-          "Date": "2012-12",
-          "scales": 1065
-        },
-        {
-          "Date": "2013-01",
-          "scales": 1255
-        },
-        {
-          "Date": "2013-02",
-          "scales": 1429
-        },
-        {
-          "Date": "2013-03",
-          "scales": 1398
-        },
-        {
-          "Date": "2013-04",
-          "scales": 1678
-        },
-        {
-          "Date": "2013-05",
-          "scales": 1524
-        },
-        {
-          "Date": "2013-06",
-          "scales": 1688
-        },
-        {
-          "Date": "2013-07",
-          "scales": 1500
-        },
-        {
-          "Date": "2013-08",
-          "scales": 1670
-        },
-        {
-          "Date": "2013-09",
-          "scales": 1734
-        },
-        {
-          "Date": "2013-10",
-          "scales": 1699
-        },
-        {
-          "Date": "2013-11",
-          "scales": 1508
-        },
-        {
-          "Date": "2013-12",
-          "scales": 1680
-        },
-        {
-          "Date": "2014-01",
-          "scales": 1750
-        },
-        {
-          "Date": "2014-02",
-          "scales": 1602
-        },
-        {
-          "Date": "2014-03",
-          "scales": 1834
-        },
-        {
-          "Date": "2014-04",
-          "scales": 1722
-        },
-        {
-          "Date": "2014-05",
-          "scales": 1430
-        },
-        {
-          "Date": "2014-06",
-          "scales": 1280
-        },
-        {
-          "Date": "2014-07",
-          "scales": 1367
-        },
-        {
-          "Date": "2014-08",
-          "scales": 1155
-        },
-        {
-          "Date": "2014-09",
-          "scales": 1289
-        },
-        {
-          "Date": "2014-10",
-          "scales": 1104
-        },
-        {
-          "Date": "2014-11",
-          "scales": 1246
-        },
-        {
-          "Date": "2014-12",
-          "scales": 1098
-        },
-        {
-          "Date": "2015-01",
-          "scales": 1189
-        },
-        {
-          "Date": "2015-02",
-          "scales": 1276
-        },
-        {
-          "Date": "2015-03",
-          "scales": 1033
-        },
-        {
-          "Date": "2015-04",
-          "scales": 956
-        },
-        {
-          "Date": "2015-05",
-          "scales": 845
-        },
-        {
-          "Date": "2015-06",
-          "scales": 1089
-        },
-        {
-          "Date": "2015-07",
-          "scales": 944
-        },
-        {
-          "Date": "2015-08",
-          "scales": 1043
-        },
-        {
-          "Date": "2015-09",
-          "scales": 893
-        },
-        {
-          "Date": "2015-10",
-          "scales": 840
-        },
-        {
-          "Date": "2015-11",
-          "scales": 934
-        },
-        {
-          "Date": "2015-12",
-          "scales": 810
-        },
-        {
-          "Date": "2016-01",
-          "scales": 782
-        },
-        {
-          "Date": "2016-02",
-          "scales": 1089
-        },
-        {
-          "Date": "2016-03",
-          "scales": 745
-        },
-        {
-          "Date": "2016-04",
-          "scales": 680
-        },
-        {
-          "Date": "2016-05",
-          "scales": 802
-        },
-        {
-          "Date": "2016-06",
-          "scales": 697
-        },
-        {
-          "Date": "2016-07",
-          "scales": 583
-        },
-        {
-          "Date": "2016-08",
-          "scales": 456
-        },
-        {
-          "Date": "2016-09",
-          "scales": 524
-        },
-        {
-          "Date": "2016-10",
-          "scales": 398
-        },
-        {
-          "Date": "2016-11",
-          "scales": 278
-        },
-        {
-          "Date": "2016-12",
-          "scales": 195
-        },
-        {
-          "Date": "2017-01",
-          "scales": 145
-        },
-        {
-          "Date": "2017-02",
-          "scales": 207
-        }
-      ]);
+  useEffect(() => {
+    function defaultSetting() {
+      // setting 시 category가 순차적으로 나와야 함
+      // 여러 데이터를 array로 받기
+      setData(
+        [
+          {
+            "date": "2018/8/1",
+            "type": "download",
+            "value": 46
+          },
+          {
+            "date": "2018/8/1",
+            "type": "register",
+            "value": 22
+          },
+          {
+            "date": "2018/8/1",
+            "type": "bill",
+            "value": 18
+          },
+          {
+            "date": "2018/8/2",
+            "type": "download",
+            "value": 61
+          },
+          {
+            "date": "2018/8/2",
+            "type": "register",
+            "value": 20
+          },
+          {
+            "date": "2018/8/2",
+            "type": "bill",
+            "value": 25
+          },
+          {
+            "date": "2018/8/3",
+            "type": "download",
+            "value": 50
+          },
+          {
+            "date": "2018/8/3",
+            "type": "register",
+            "value": 29
+          },
+          {
+            "date": "2018/8/3",
+            "type": "bill",
+            "value": 28
+          },
+          {
+            "date": "2018/8/4",
+            "type": "download",
+            "value": 62
+          },
+          {
+            "date": "2018/8/4",
+            "type": "register",
+            "value": 45
+          },
+          {
+            "date": "2018/8/4",
+            "type": "bill",
+            "value": 42
+          },
+          {
+            "date": "2018/8/5",
+            "type": "download",
+            "value": 64
+          },
+          {
+            "date": "2018/8/5",
+            "type": "register",
+            "value": 82
+          },
+          {
+            "date": "2018/8/5",
+            "type": "bill",
+            "value": 61
+          },
+          {
+            "date": "2018/8/6",
+            "type": "download",
+            "value": 18
+          },
+          {
+            "date": "2018/8/6",
+            "type": "register",
+            "value": 20
+          },
+          {
+            "date": "2018/8/6",
+            "type": "bill",
+            "value": 87
+          },
+          {
+            "date": "2018/8/7",
+            "type": "download",
+            "value": 42
+          },
+          {
+            "date": "2018/8/7",
+            "type": "register",
+            "value": 19
+          },
+          {
+            "date": "2018/8/7",
+            "type": "bill",
+            "value": 70
+          },
+          {
+            "date": "2018/8/8",
+            "type": "download",
+            "value": 29
+          },
+          {
+            "date": "2018/8/8",
+            "type": "register",
+            "value": 23
+          },
+          {
+            "date": "2018/8/8",
+            "type": "bill",
+            "value": 38
+          },
+          {
+            "date": "2018/8/9",
+            "type": "download",
+            "value": 38
+          },
+          {
+            "date": "2018/8/9",
+            "type": "register",
+            "value": 29
+          },
+          {
+            "date": "2018/8/9",
+            "type": "bill",
+            "value": 48
+          },
+          {
+            "date": "2018/8/10",
+            "type": "download",
+            "value": 36
+          },
+          {
+            "date": "2018/8/10",
+            "type": "register",
+            "value": 67
+          },
+          {
+            "date": "2018/8/10",
+            "type": "bill",
+            "value": 50
+          },
+          {
+            "date": "2018/8/11",
+            "type": "download",
+            "value": 43
+          },
+          {
+            "date": "2018/8/11",
+            "type": "register",
+            "value": 31
+          },
+          {
+            "date": "2018/8/11",
+            "type": "bill",
+            "value": 54
+          },
+          {
+            "date": "2018/8/12",
+            "type": "download",
+            "value": 41
+          },
+          {
+            "date": "2018/8/12",
+            "type": "register",
+            "value": 34
+          },
+          {
+            "date": "2018/8/12",
+            "type": "bill",
+            "value": 45
+          },
+          {
+            "date": "2018/8/13",
+            "type": "download",
+            "value": 64
+          },
+          {
+            "date": "2018/8/13",
+            "type": "register",
+            "value": 28
+          },
+          {
+            "date": "2018/8/13",
+            "type": "bill",
+            "value": 68
+          },
+          {
+            "date": "2018/8/14",
+            "type": "download",
+            "value": 16
+          },
+          {
+            "date": "2018/8/14",
+            "type": "register",
+            "value": 47
+          },
+          {
+            "date": "2018/8/14",
+            "type": "bill",
+            "value": 28
+          },
+          {
+            "date": "2018/8/15",
+            "type": "download",
+            "value": 44
+          },
+          {
+            "date": "2018/8/15",
+            "type": "register",
+            "value": 43
+          },
+          {
+            "date": "2018/8/15",
+            "type": "bill",
+            "value": 17
+          }
+        ]);
     }
     defaultSetting();
-  },[]);
+  }, []);
 
-  const {RangePicker} = DatePicker;
+  /* 그룹 데이터 들어왔을 때, 가장 큰 날짜 구하기
+  useEffect(()=>{
+    if(data.length>0){
+      let maxDate = 0;
+      data.map((d)=>{
+        const partialMax = Date.parse(d[d.length-1].date);
+        if(d.length>0 && partialMax>maxDate){
+          maxDate = partialMax;
+        }
+      });
+      setGraphDate(maxDate);
+    }
+  },[data]);
+  */
 
-  function setGraphDate(time, timeString){
-    console.dir(time);
-    console.dir(timeString);
+  useEffect(() => {
+    if (data.length > 0) {
+      setGraphDate(data[data.length - 1].date);
+    }
+  }, [data])
+
+  function selectGraphDate(timeString) {
+    if (timeString === null) { // 날짜를 삭제해도 기존 날짜로 유지
+      return;
+    }
+    const selDate = new Date(timeString);
+    const firstDateNum = new Date(data[0].date);
+    const lastDateNum = new Date(data[data.length - 1].date);
+
+    const wholeDateLength = lastDateNum - firstDateNum;
+    const selDateLength = selDate - firstDateNum;
+    setGraphDate(selDate);
+    setGraphRate(parseFloat((selDateLength / wholeDateLength).toFixed(1))); // %로 나타내기
+  }
+
+  function gotoGoalSet() {
+    console.log("Hi");
+    window.location.href = "/GoalSet";
   }
 
   return (
     <Col>
       <PageHeader
-        title={name+"님의 목표 달성률"}
+        title={name + "님의 목표 달성률"}
         subtitle="목표 달성치를 그래프로 한 눈에 볼 수 있어요."
-        />
+      />
       <Col className="graph-con">
-        <div className="select-date-con">
-          <div class="date-title"><CalendarOutlined/><p>날짜 선택</p></div>
-          <div class="date-select">
-            <RangePicker onChange={setGraphDate}/>
+        <div className="graph-header">
+          <div className="select-date-con">
+            <div className="date-title"><CalendarOutlined /><p>날짜 선택</p></div>
+            <div className="date-select">
+              {graphDate !== undefined ? <DatePicker defaultValue={moment(graphDate)} onChange={selectGraphDate} /> : "ㄱㄷ"}
+            </div>
           </div>
-        </div>
-        <div>
-          {data.length!==0?<Line {...config} />:undefined}
+          <Tabs defaultActiveKey="1" className="graph-tab-con" tabBarExtraContent={operations}>
+            <TabPane tab="Area" key="1">
+              <div>
+                {data.length !== 0 ? <Area {...config} /> : undefined}
+              </div>
+            </TabPane>
+            <TabPane tab="Line" key="2">
+              <div>
+                {data.length !== 0 ? <Line {...config} /> : undefined}
+              </div>
+            </TabPane>
+            <TabPane tab="Bar" key="3">
+              <div>
+                {data.length !== 0 ? <Bar {...config} /> : undefined}
+              </div>
+            </TabPane>
+          </Tabs>
         </div>
       </Col>
     </Col>
