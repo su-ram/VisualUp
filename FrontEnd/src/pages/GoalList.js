5e16a41323bbbfd35c866d47f73e2f5a39c527fa
Cho-Eunhak <02069@naver.com>
2020-10-04 10:14:12 +0900

    useeffect하기전 commit

--- FrontEnd/src/pages/GoalList.js
+++ FrontEnd/src/pages/GoalList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Card, Col, Row, Pagination } from 'antd';
 import {  EllipsisOutlined } from '@ant-design/icons';
 import styles from './GoalList.module.css';
