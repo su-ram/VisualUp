e6b8ba3d33290b922e5bf8461c05ea50d878d446
heeeun <01072417732w@gmail.com>
2020-10-05 22:13:51 +0900

    Change Visaulize page structure to connect with DB

--- FrontEnd/src/pages/Visualize.js
+++ FrontEnd/src/pages/Visualize.js
@@ -5,253 +5,44 @@ import { Col, DatePicker, Tabs, Button } from 'antd';
 import { CalendarOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import "./Visualize.css";
+import GaugeLayer from '@antv/g2plot/lib/plots/gauge/layer';
 
 const { TabPane } = Tabs;
 
 function Visualize() {
   const range = 0.6; // 나중에 비율 조정
+  const [name, setName] = useState("");
   const [dataSet, setDataSet] = useState({});
   const [graphDate, setGraphDate] = useState(undefined);
   const [graphRate, setGraphRate] = useState(1 - range / 2);
+  const [graphConfig, setGraphConfig] = useState({});
 
-  const name = "홍미주";
   const operations = <Button onClick={gotoGoalSet}>Add Goal</Button>;
 
   useEffect(() => {
-    function defaultSetting() {
-      // setting 시 category가 순차적으로 나와야 함
-      // 여러 데이터를 array로 받기
-      setDataSet(
+    // setting 시 category가 순차적으로 나와야 함
+    // 여러 데이터를 array로 받기
+    getGoalDataFromDB();
+  }, []);
+
+  async function getGoalDataFromDB() {
+    // db에서 해당 목표 정보 받아오기
+    const dbData = {
+      "userId": 1,
+      "userName": "홍미주",
+      "goals": [
         {
-          "group": [
-            {
-              "date": "2018/8/1",
-              "type": "git commit",
-              "value": 4623
-            },
-            {
-              "date": "2018/8/1",
-              "type": "coding test",
-              "value": 2208
-            },
-            {
-              "date": "2018/8/1",
-              "type": "cookie project",
-              "value": 182
-            },
-            {
-              "date": "2018/8/2",
-              "type": "git commit",
-              "value": 6145
-            },
-            {
-              "date": "2018/8/2",
-              "type": "coding test",
-              "value": 2016
-            },
-            {
-              "date": "2018/8/2",
-              "type": "cookie project",
-              "value": 257
-            },
-            {
-              "date": "2018/8/3",
-              "type": "git commit",
-              "value": 508
-            },
-            {
-              "date": "2018/8/3",
-              "type": "coding test",
-              "value": 2916
-            },
-            {
-              "date": "2018/8/3",
-              "type": "cookie project",
-              "value": 289
-            },
-            {
-              "date": "2018/8/4",
-              "type": "git commit",
-              "value": 6268
-            },
-            {
-              "date": "2018/8/4",
-              "type": "coding test",
-              "value": 4512
-            },
-            {
-              "date": "2018/8/4",
-              "type": "cookie project",
-              "value": 428
-            },
-            {
-              "date": "2018/8/5",
-              "type": "git commit",
-              "value": 6411
-            },
-            {
-              "date": "2018/8/5",
-              "type": "coding test",
-              "value": 8281
-            },
-            {
-              "date": "2018/8/5",
-              "type": "cookie project",
-              "value": 619
-            },
-            {
-              "date": "2018/8/6",
-              "type": "git commit",
-              "value": 1890
-            },
-            {
-              "date": "2018/8/6",
-              "type": "coding test",
-              "value": 2008
-            },
-            {
-              "date": "2018/8/6",
-              "type": "cookie project",
-              "value": 87
-            },
-            {
-              "date": "2018/8/7",
-              "type": "git commit",
-              "value": 4251
-            },
-            {
-              "date": "2018/8/7",
-              "type": "coding test",
-              "value": 1963
-            },
-            {
-              "date": "2018/8/7",
-              "type": "cookie project",
-              "value": 706
-            },
-            {
-              "date": "2018/8/8",
-              "type": "git commit",
-              "value": 2978
-            },
-            {
-              "date": "2018/8/8",
-              "type": "coding test",
-              "value": 2367
-            },
-            {
-              "date": "2018/8/8",
-              "type": "cookie project",
-              "value": 387
-            },
-            {
-              "date": "2018/8/9",
-              "type": "git commit",
-              "value": 3880
-            },
-            {
-              "date": "2018/8/9",
-              "type": "coding test",
-              "value": 2956
-            },
-            {
-              "date": "2018/8/9",
-              "type": "cookie project",
-              "value": 488
-            },
-            {
-              "date": "2018/8/10",
-              "type": "git commit",
-              "value": 3606
-            },
-            {
-              "date": "2018/8/10",
-              "type": "coding test",
-              "value": 678
-            },
-            {
-              "date": "2018/8/10",
-              "type": "cookie project",
-              "value": 507
-            },
-            {
-              "date": "2018/8/11",
-              "type": "git commit",
-              "value": 4311
-            },
-            {
-              "date": "2018/8/11",
-              "type": "coding test",
-              "value": 3188
-            },
-            {
-              "date": "2018/8/11",
-              "type": "cookie project",
-              "value": 548
-            },
-            {
-              "date": "2018/8/12",
-              "type": "git commit",
-              "value": 4116
-            },
-            {
-              "date": "2018/8/12",
-              "type": "coding test",
-              "value": 3491
-            },
-            {
-              "date": "2018/8/12",
-              "type": "cookie project",
-              "value": 456
-            },
-            {
-              "date": "2018/8/13",
-              "type": "git commit",
-              "value": 6419
-            },
-            {
-              "date": "2018/8/13",
-              "type": "coding test",
-              "value": 2852
-            },
-            {
-              "date": "2018/8/13",
-              "type": "cookie project",
-              "value": 689
-            },
-            {
-              "date": "2018/8/14",
-              "type": "git commit",
-              "value": 1643
-            },
-            {
-              "date": "2018/8/14",
-              "type": "coding test",
-              "value": 4788
-            },
-            {
-              "date": "2018/8/14",
-              "type": "cookie project",
-              "value": 280
-            },
-            {
-              "date": "2018/8/15",
-              "type": "git commit",
-              "value": 445
-            },
-            {
-              "date": "2018/8/15",
-              "type": "coding test",
-              "value": 4319
-            },
-            {
-              "date": "2018/8/15",
-              "type": "cookie project",
-              "value": 176
-            }
-          ],
-          "set1":
-            [
+          "goalId": 1,
+          "title": "목표 제목", //
+          "startDate": new Date(),
+          "endDate": new Date(),
+          "term": 2,
+          "hashtags": ["hashtag1", "hashtag2", "hashtag3"],
+          "isOpened": true,
+          "target": "주기별 목표량",
+          "template": "Line", //
+          "graphColor": "#ade09d", //
+          "dataSet":[
               {
                 "date": "2018/8/1",
                 "type": "coding test",
@@ -327,93 +118,144 @@ function Visualize() {
                 "type": "coding test",
                 "value": 43
               }
-            ],
-          "set2":
-            [
-              {
-                "date": "2018/8/1",
-                "type": "git commit",
-                "value": 46
-              },
-              {
-                "date": "2018/8/2",
-                "type": "git commit",
-                "value": 61
-              },
-              {
-                "date": "2018/8/3",
-                "type": "git commit",
-                "value": 50
-              },
-              {
-                "date": "2018/8/4",
-                "type": "git commit",
-                "value": 62
-              },
-              {
-                "date": "2018/8/5",
-                "type": "git commit",
-                "value": 64
-              },
-              {
-                "date": "2018/8/6",
-                "type": "git commit",
-                "value": 18
-              },
-              {
-                "date": "2018/8/7",
-                "type": "git commit",
-                "value": 42
-              },
-              {
-                "date": "2018/8/8",
-                "type": "git commit",
-                "value": 29
-              },
-              {
-                "date": "2018/8/9",
-                "type": "git commit",
-                "value": 38
-              },
-              {
-                "date": "2018/8/10",
-                "type": "git commit",
-                "value": 36
-              },
-              {
-                "date": "2018/8/11",
-                "type": "git commit",
-                "value": 43
-              },
-              {
-                "date": "2018/8/12",
-                "type": "git commit",
-                "value": 41
-              },
-              {
-                "date": "2018/8/13",
-                "type": "git commit",
-                "value": 64
-              },
-              {
-                "date": "2018/8/14",
-                "type": "git commit",
-                "value": 16
-              },
-              {
-                "date": "2018/8/15",
-                "type": "git commit",
-                "value": 44
-              }
-            ]
+            ], //
+          "dailyData": [
+            {
+              "dailyId": 1,
+              "todayDate": new Date(),
+              "isDone": true,
+              "WhatIdone": "오늘 한 일",
+              "stars": 5,
+            }
+          ]
+        },{
+          "goalId": 2,
+          "title": "목표 제목2", //
+          "startDate": new Date(),
+          "endDate": new Date(),
+          "term": 2,
+          "hashtags": ["hashtag1", "hashtag2", "hashtag3"],
+          "isOpened": true,
+          "target": "주기별 목표량",
+          "template": "Bar", //
+          "graphColor": "#e09da4", //
+          "dataSet":[
+            {
+              "date": "2018/8/1",
+              "type": "git commit",
+              "value": 46
+            },
+            {
+              "date": "2018/8/2",
+              "type": "git commit",
+              "value": 61
+            },
+            {
+              "date": "2018/8/3",
+              "type": "git commit",
+              "value": 50
+            },
+            {
+              "date": "2018/8/4",
+              "type": "git commit",
+              "value": 62
+            },
+            {
+              "date": "2018/8/5",
+              "type": "git commit",
+              "value": 64
+            },
+            {
+              "date": "2018/8/6",
+              "type": "git commit",
+              "value": 18
+            },
+            {
+              "date": "2018/8/7",
+              "type": "git commit",
+              "value": 42
+            },
+            {
+              "date": "2018/8/8",
+              "type": "git commit",
+              "value": 29
+            },
+            {
+              "date": "2018/8/9",
+              "type": "git commit",
+              "value": 38
+            },
+            {
+              "date": "2018/8/10",
+              "type": "git commit",
+              "value": 36
+            },
+            {
+              "date": "2018/8/11",
+              "type": "git commit",
+              "value": 43
+            },
+            {
+              "date": "2018/8/12",
+              "type": "git commit",
+              "value": 41
+            },
+            {
+              "date": "2018/8/13",
+              "type": "git commit",
+              "value": 64
+            },
+            {
+              "date": "2018/8/14",
+              "type": "git commit",
+              "value": 16
+            },
+            {
+              "date": "2018/8/15",
+              "type": "git commit",
+              "value": 44
+            }
+          ], //
+          "dailyData": [
+            {
+              "dailyId": 1,
+              "todayDate": new Date(),
+              "isDone": true,
+              "WhatIdone": "오늘 한 일",
+              "stars": 4,
+            }
+          ]
         }
-      );
+      ]
     }
-    defaultSetting();
-  }, []);
+
+    const tmpConfig = {};
+    const tmpData = {"group": []};
+
+    await dbData.goals.map((goal)=>{
+      tmpConfig[goal.title]={
+        "template" : goal.template,
+        "graphColor" : goal.graphColor
+      };
+      tmpData[goal.title]=goal.dataSet;
+      goal.dataSet.map((data)=>{
+        tmpData["group"].push(data);
+      })
+    })
+    tmpConfig["group"] = {
+      "template" : "Area",
+      "graphColor" : ["#e09da4","#ade09d","#9db8e0"],
+    }
+
+    // 개별 graph setting
+    await setName(dbData.userName);
+    await setGraphConfig(tmpConfig);
+    await setDataSet(tmpData);
+
+  }
 
   useEffect(() => { // 데이터 중, 가장 큰 날짜 구하기 => default 세팅
-    function defaultGroupDate(){
+    function defaultGroupDate() {
       if (Object.keys(dataSet).length > 0) {
         const arr = dataSet["group"];
         setGraphDate(arr[arr.length - 1].date);
@@ -433,17 +275,17 @@ function Visualize() {
     const groupData = dataSet["group"];
     const firstDate = groupData[0].date;
     const firstDateNum = new Date(firstDate);
-    const lastDate = groupData[groupData.length-1].date;
+    const lastDate = groupData[groupData.length - 1].date;
     const lastDateNum = new Date(lastDate);
 
     const wholeDateLength = lastDateNum - firstDateNum;
     const selDateLength = selDate - firstDateNum;
 
-    if(wholeDateLength<selDateLength){ // 선택된 날짜가 마지막 날짜 이후라면
-      alert( `마지막 기록 날짜(${lastDate}) 이전의 날짜를 선택해주세요.`);
+    if (wholeDateLength < selDateLength) { // 선택된 날짜가 마지막 날짜 이후라면
+      alert(`마지막 기록 날짜(${lastDate}) 이전의 날짜를 선택해주세요.`);
       return;
     }
-    if(selDateLength<0){
+    if (selDateLength < 0) {
       alert(`첫 기록 날짜(${firstDate}) 이후의 날짜를 선택해주세요.`);
       return;
     }
@@ -456,19 +298,19 @@ function Visualize() {
     window.location.href = "/GoalSet";
   }
 
-  function getConfig(type) {
-    const data = dataSet[type];
+  function getConfig(title) {
+    const data = dataSet[title];
     const config = {
       data, // 이름이 무조건 data여야함
       //width: '100',
       xField: 'date', // xfield에 적용할 변수
       yField: 'value', // yfield에 적용할 변수
       forceFit: true,
-      color: ['#E9BE78', '#6a93c5', '#6ac593'], // 선 색깔 지정
+      color: graphConfig[title].graphColor, // 선 색깔 지정
       seriesField: 'type', // 클릭 시 해당 그래프만 나타남
       legend: {
-        //visible: false,
-        //legend: { position: 'bottom-left' },
+        visible: true,
+        position: 'bottom-left',
       },
       xAxis: {
         type: 'dateTime', // x축 표시 형식
@@ -503,13 +345,12 @@ function Visualize() {
     return config;
   }
 
-  function renderGraph(graphType, dataType){
-    if (dataSet.length === 0)
-      return undefined;
-    switch(graphType){
-      case "Area" : return <Area {...getConfig(dataType)}/>
-      case "Line" : return <Line {...getConfig(dataType)}/>
-      case "Bar" : return <Bar {...getConfig(dataType)}/>
+  function renderGraph(title) {
+    const type = graphConfig[title].template;
+    switch (type) {
+      case "Area": return <Area {...getConfig(title)} />
+      case "Line": return <Line {...getConfig(title)} />
+      case "Bar": return <Bar {...getConfig(title)} />
     }
   }
 
@@ -528,21 +369,16 @@ function Visualize() {
             </div>
           </div>
           <Tabs defaultActiveKey="1" className="graph-tab-con" tabBarExtraContent={operations}>
-            <TabPane tab="All" key="1">
-              <div>
-                {renderGraph("Area", "group")}
-              </div>
-            </TabPane>
-            <TabPane tab="set1" key="2">
-              <div>
-                {renderGraph("Line", "set1")}
-              </div>
-            </TabPane>
-            <TabPane tab="set2" key="3">
-              <div>
-                {renderGraph("Line", "set2")}
-              </div>
-            </TabPane>
+            {
+              Object.keys(dataSet).length>0?
+                Object.keys(dataSet).map((title, index)=>
+                  <TabPane tab={title} key={index}>
+                    <div>
+                      {renderGraph(title)}
+                    </div>
+                  </TabPane>
+                ):"로딩중입니다..."
+            }
           </Tabs>
         </div>
       </Col>
