2ad0a73f41bb148f7df2f5eae91579a12299e4f2
lim-jeongmin <62532871+lim-jeongmin@users.noreply.github.com>
2020-10-09 14:14:19 +0900

    component에 link 연결 코드 추가

--- FrontEnd/src/components/FourthBox.js
+++ FrontEnd/src/components/FourthBox.js
@@ -6,11 +6,11 @@ import { getIcon } from '../components/Icon';
 
 
 function FourthBox(props){
-    const url = props.img
+    const img_url = props.img
     return (
         <div className = "fourth-box-content">
             <div className="only-text">
-                <img className="fourth-img" src = {url}/>   {/* 컴포넌트로 이미지 넣기 */}
+                <img className="fourth-img" src = {img_url}/>   {/* 컴포넌트로 이미지 넣기 */}
                 <div className="fourth-name"><h3>{props.name}</h3></div>
                 <div className="fourth-part"><h5>{props.part}</h5></div>
                 {/* <Link to= {props.insta}>
@@ -22,10 +22,15 @@ function FourthBox(props){
                 <Link to= {props.blog}>
                     <span className="internet" >{getIcon("internet")}</span>
                 </Link> */}
-                <InstagramOutlined style ={{fontSize : '18px', color : '#5C411D', padding:'3px'}}/>
-                <FacebookOutlined style ={{fontSize : '18px', color : '#5C411D', padding:'3px'}}/>
-                <span className="internet" >{getIcon("internet")}</span>
-
+                <a href = {props.insta} >
+                 <InstagramOutlined style ={{fontSize : '18px', color : '#5C411D', padding:'3px'}}/>
+                </a>
+                <a href = {props.facebook} >
+                 <FacebookOutlined style ={{fontSize : '18px', color : '#5C411D', padding:'3px'}}/>
+                </a>
+                <a href = {props.blog} >
+                    <span className="internet" >{getIcon("internet")}</span>
+                </a>
 
                 {/* <GithubOutlined style ={{fontSize : '18px', color : '#5C411D', padding:'3px'}}/> */}
             </div>
