8b74e8e399374b23b279965272bf8d534b04331a
heeeun <01072417732w@gmail.com>
2020-10-09 16:33:08 +0900

    visualize.js 구조 바꾸기 => 모듈화

--- FrontEnd/src/components/index.js
+++ FrontEnd/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as PageHeader } from './PageHeader';
 export { default as Graph } from './Graph';
+export { default as DailyCheck } from './DailyCheck';
 export { default as FourthBox } from './FourthBox';
