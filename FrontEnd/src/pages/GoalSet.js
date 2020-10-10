fb4405ba5c6abbfb30f948cf5978ab7487d55396
Cho-Eunhak <02069@naver.com>
2020-10-10 15:55:28 +0900

    목표설정, 목표리스트 api post get 호출 초안 커밋

--- FrontEnd/src/pages/GoalSet.js
+++ FrontEnd/src/pages/GoalSet.js
@@ -1,83 +1,92 @@
+
 import React, {useState} from 'react';
-import { Input, Col, Row, Checkbox, InputNumber, DatePicker, Switch } from 'antd';
+import axios from 'axios';
+import { Col, Row, Checkbox, DatePicker, Switch } from 'antd';
 import "antd/dist/antd.css";
 import styles from './GoalSet.module.css';
 import { LineChartOutlined, BarChartOutlined, PieChartOutlined, AlignLeftOutlined, EditOutlined, CheckOutlined, CalendarOutlined, BarsOutlined, PushpinOutlined } from '@ant-design/icons'
 
-
-function onDateChange(value) {
-    console.log('changed', value);
-  }
-
-function onChange(checkedValues) {
-    console.log('checked = ', checkedValues);
-  }
+  
 function GoalSet(){
-    const [inputs, setInputs] = useState({
-        set: '',
-        setsize: '',
-        date:'',
-        period:'',
-        hashtag1:'',
-        hashtag2:'',
-        hashtag3:'',
-        hashtag4:'',
-        graph:'',
-        checked:false
-    });
+    const [goal, setgoal] = useState("");
+    const [date, setdate] = useState("");
+    const [period, setperiod] = useState("");
+    const [periodgoal, setperiodgoal] = useState("");
+    const [hashtag1, sethashtag1] = useState("");
+    const [hashtag2, sethashtag2] = useState("");
+    const [hashtag3, sethashtag3] = useState("");
+    const [hashtag4, sethashtag4] = useState("");
+    const [graph, setgraph] = useState("");
+    const [graphcolor, setgraphcolor] = useState("");
+    const [checked, setchecked] = useState(false);
 
-    const { set, setsize, date, period, hashtag1, hashtag2, hashtag3, hashtag4, graph } = inputs;
-
-    const onvaluechange = (e) => {
-        const { value, name } = e.target;
-        setInputs({
-          ...inputs,
-          set: value 
-        });
-      };
+    const onChangegoal = e => setgoal(e.target.value);
+    const onChangedate = e => setdate(e.target.value);
+    const onDateChange = e => setperiod(e.target.value);
+    const onChangeperiodgoal = e => setperiodgoal(e.target.value);
+    const onChangehashtag1 = e => sethashtag1(e.target.value);
+    const onChangehashtag2 = e => sethashtag2(e.target.value);
+    const onChangehashtag3 = e => sethashtag3(e.target.value);
+    const onChangehashtag4 = e => sethashtag4(e.target.value);
+    function onChangegraph(checkedValues) {
+        console.log('checked = ', checkedValues);
+      }
+    const onChangegraphcolor = e => setgraphcolor(e.target.value);
 
     const options = [
         { label: '파이 그래프', value: '파이 그래프' },
         { label: '선 그래프', value: '선 그래프' },
         { label: '막대 그래프', value: '막대 그래프' },
       ];
+      const api= "http://visualup.koreacentral.cloudapp.azure.com:8080 /goal"
+      const onSubmit = async() => {
+        try{
+        const status= {
+        title: goal,
+        date:date,
+        term:period,
+        //startdate, enddate:period,, graphcolor추가해야함, hashtag 묶어야지
+        termgoal:periodgoal,
+        hashtags:hashtag1+","+hashtag2+","+hashtag3+","+hashtag4,
+        template:graph,
+        open:false
+      };
+      //const { data: post } = await axios.post(api, status);
+      console.log(status);
+      if (status === 201) {
+        alert("목표가 등록되었습니다.");
+      }} catch (e) {
+        alert("userid가 없습니다.");
+      } 
+    }
     
+
     return(
-        <div className ="page-set">
+        <div>
         <h1 className={styles.mainTitle}>목표 입력</h1>
             <Col align="middle" className={styles.sub}><p className={styles.subSub}>본인이 달성할 목표를 상세하게 적어주세요.</p></Col>
         <Row justify="center" >
             <Col span={9} offset={3}>
-                    <Input.Group className="GoalSet-input">
-                        <Col span={13}>
-                        <p className={styles.name}><EditOutlined />목표</p>
-                        <br />
-                        <Input placeholder="ex)코딩 테스트 풀기" onvaluechange={onvaluechange} value={set}/>
-                        </Col>
-                        <br />
-                        <br />
-                        <Col span={13}>
-                        <p className={styles.name}><CheckOutlined />목표량</p>
-                        <br />
-                        <Input placeholder="3"  onvaluechange={onvaluechange} value={setsize}/>
-                        </Col>
-                        <br />
-                        <br />
-                        <Col span={15}>
-                        <p className={styles.name}><CalendarOutlined />날짜 선택</p>
-                        <br />
-                        <br />
-                        <DatePicker.RangePicker  onvaluechange={onvaluechange} value={date} style={{ width: '100%' }} />
-                        </Col>
-                        <br />
-                        <Col span={13}>
-                        <p className={styles.name}><BarsOutlined />주기
-                        <br />
-                        <InputNumber  onvaluechange={onvaluechange} value={period} min={1} max={10} defaultValue={3} onChange={onDateChange} />
-                        일마다
-                        </p>
-                         </Col>
-                    </Input.Group>
+                <Col span={12}>
+                <p className={styles.name}><EditOutlined />목표</p>
+                <br />
+                <input className={styles.input} placeholder="ex)코딩 테스트 풀기" onChange={onChangegoal} value={goal}/>
+                <br />
+                <br />
+                <p className={styles.name}><CalendarOutlined />날짜 선택</p>
+                <br />
+                <br />
+                <DatePicker.RangePicker  onChange={onChangedate} value={date} style={{ width: '100%' }} />
+                <br />
+                <p className={styles.name}><BarsOutlined />주기
+                <br />
+                <input className={styles.input} value={period} placeholder="ex)3" onChange={onDateChange} />
+                일마다
+                </p>
+                <p className={styles.name}><EditOutlined />주기별 목표</p>
+                <br />
+                <input className={styles.input} placeholder="ex)코딩 테스트 문제 5개 풀기" onChange={onChangeperiodgoal} value={periodgoal}/>
+                </Col>
             
             </Col>
             <Col span={12}>
@@ -87,21 +96,21 @@ function GoalSet(){
                 </Col>
                 <Row>
                     <Col span={8}>
-                        <Input onvaluechange={onvaluechange} value={hashtag1} placeholder="ex)코테"/>
-                        <Input onvaluechange={onvaluechange} value={hashtag2} placeholder="ex)C++"/>
+                        <input className={styles.input} onChange={onChangehashtag1} value={hashtag1} placeholder="ex)코테"/>
+                        <input className={styles.input} onChange={onChangehashtag2} value={hashtag2} placeholder="ex)C++"/>
                     </Col>
                     <br />
                     <br />
                     <Col span={8}>
-                        <Input onvaluechange={onvaluechange} value={hashtag3} placeholder="ex)자바"/>
-                        <Input onvaluechange={onvaluechange} value={hashtag4} placeholder="ex)취준"/>
+                        <input className={styles.input} onChange={onChangehashtag3} value={hashtag3} placeholder="ex)자바"/>
+                        <input className={styles.input} onChange={onChangehashtag4} value={hashtag4} placeholder="ex)취준"/>
                     </Col>
                 </Row>
                 <Col span={15}>
                 <div className={styles.name}><AlignLeftOutlined />템플릿 설정</div>
                 </Col>
                 <Col span={12}>
-                <Checkbox.Group className={styles.graphname} value={graph} options={options} defaultValue={['파이 그래프']} onChange={onChange}>
+                <Checkbox.Group className={styles.graphname} value={graph} options={options} defaultValue={['파이 그래프']} onChange={onChangegraph}>
                 </Checkbox.Group>
                 <br />
                 <br />
@@ -122,10 +131,10 @@ function GoalSet(){
                 </Col>
                 <Col span={24}>
                 <div className={styles.graphdesc}>공개
-                <Switch onClick={() => setInputs(!inputs.checked)} checked={inputs.checked} />비공개</div>
+                <Switch onClick={() => setchecked(!checked)} checked={checked} />비공개</div>
                 </Col>
                 <Col span={24}>
-                <button type="button" className={styles.btn1}><p className={styles.font}>등록하기</p> </button>
+                <button type="button" onClick={onSubmit} className={styles.btn1}><p className={styles.font}>등록하기</p> </button>
                 <button type="button" className={styles.btn2}><p className={styles.font}>취소 </p> </button>
                 </Col>
             
