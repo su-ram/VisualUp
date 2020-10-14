1e1ca37c6d588ed14769cd87dea87c3454156b54
JaneseoHyeon <49301073+JaneseoHyeon@users.noreply.github.com>
2020-10-15 04:58:03 +0900

    ㄹ

---
+++ FrontEnd/src/components/Pagination2.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Pagination2 = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map(number => (
+          <li key={number} >
+            <a onClick={(e) => paginate(e, number)} href="#" >
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination2;
