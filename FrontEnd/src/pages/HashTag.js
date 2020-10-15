081961c8769350bb50556c51b46ebe9402847606
JaneseoHyeon <49301073+JaneseoHyeon@users.noreply.github.com>
2020-10-15 22:22:06 +0900

    HashTag검색수정

--- FrontEnd/src/pages/HashTag.js
+++ FrontEnd/src/pages/HashTag.js
@@ -148,11 +148,14 @@ function HashTag({match}){
                     </div>
                 </div><br/>
                 <div className="page-subtitle">
-                {
-                  data.length===0?
-                  <h5>검색 결과가 없습니다.</h5>:
-                  <h5>{hashtag} 검색결과입니다.</h5>
-                }
+                  {
+                    hashtag===undefined?
+                    <h5>검색해주세요.</h5>:
+                    (data.length===0?
+                      <h5>검색 결과가 없습니다.</h5>:
+                      <h5>{hashtag} 검색결과입니다.</h5>
+                    )
+                  }                
                 </div>
             </div><br/><br/>
             <Row>
