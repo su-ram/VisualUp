bd876b90453e7735f999e00d4092a309c43d1495
SJLEE316 <lsj8249@naver.com>
2020-10-16 18:05:59 +0900

    first_Visual Up!

--- FrontEnd/src/pages/FirstComponent.js
+++ FrontEnd/src/pages/FirstComponent.js
@@ -12,7 +12,7 @@ function FirstComponent() {
   return (
     <div className="component first-component">
       <div className = "first-component-text">
-        <h4>visual up</h4>
+        <h4>Visual UP!</h4>
         <h1>코딩 연습 제대로 해보자!<br/>
         하루하루 꾸준한 습관 기르기<br/>
         목표 시각화 사이트, Visual UP</h1>
